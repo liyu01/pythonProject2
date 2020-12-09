1ad928947d27662e3db741fbe2d84ab5af583123
user <17665353261@qq.com>
2020-12-09 17:02:20 +0800

    串口工具

--- Web/test/Window.py
+++ Web/test/Window.py
@@ -2,31 +2,41 @@
 # -*- coding: UTF-8 -*-
 
 
-import tkinter
+from tkinter import *
+
+top = Tk()
+top.title("串口操作")
+top.geometry('650x650+10+10')
+menubar = Menu(top)
+Texta = Text(top, width="40", height="20", pady=0)
+Texta.grid(row=1, column=0, rowspan=10, columnspan=10)
+Textb = Text(top, width="40", height="20", pady=0)
+Textb.grid(row=1, column=12, rowspan=15, columnspan=10)
+labela = Label(top, text="Hello boy")
+labela.grid()
 
 
 def get_text():
-    print(Textb.get())
+    t = Texta.get(1.0, END)
+    Textb.delete(1.0, END)
+    Textb.insert(1.0, t)
+    return t
 
 
-top = tkinter.Tk()
-menubar = tkinter.Menu(top)
-listb = tkinter.Listbox(top, width="60", height="20")  # 创建两个列表组件
-Textb = tkinter.Text(top, width="20", height="20")
-listb1 = tkinter.Listbox(top, width="60", height="20")  # 创建两个列表组件
-button1 = tkinter.Button(top, text="点我", bg="red", activebackground="yellow", menubar=get_text)
-CheckVar1 = tkinter.IntVar()
-C1 = tkinter.Checkbutton(listb, text="RUNOOB", variable=CheckVar1, \
-                         onvalue=1, offvalue=0, height=2, \
-                         width=10)
-filemenu = tkinter.Menu(menubar, tearoff=False)
-explainmenu = tkinter.Menu(menubar, tearoff=False)
+def get_serial():
+    return
 
-listb.pack()  # 将小部件放置到主窗口中
-listb1.pack()  # 将小部件放置到主窗口中
-button1.pack()
-Textb.pack()
-C1.pack()
+
+button1 = Button(top, text="开始测试", bg="#E6E6FA", activebackground="#F8F8FF", command=get_text)
+button1.grid()
+#  复选框
+CheckVar1 = IntVar()
+C1 = Checkbutton(top, text="啥也不是", variable=CheckVar1, onvalue=1, offvalue=0, height=2, width=10)
+C1.grid()
+
+# 菜单栏
+filemenu = Menu(menubar, tearoff=False)
+explainmenu = Menu(menubar, tearoff=False)
 menubar.add_cascade(label="文件", menu=filemenu)
 menubar.add_cascade(label="说明", menu=explainmenu)
 filemenu.add_command(label="退出", command=top.quit())
